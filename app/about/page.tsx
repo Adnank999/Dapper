f06d10987157bda77e313a911520e5ff644a05da
Adnank999 <adnanhossain4ever@gmail.com>
2025-07-17 22:24:02 +0600

    removed about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ import ShuffleText from "../components/ShuffleText";
 export default function About() {
   return (
     <div className="h-[200vh]">
-      <ShuffleText />
+      {/* <ShuffleText /> */}
       
     </div>
   );
